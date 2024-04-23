c245575d9e556f06c7f5069cb7267ec1d505a559
Farid Tri Wicaksono <farid.wicak@gmail.com>
2024-04-23 08:57:59 +0700

    fix: missing define common

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const Wrapper = require('./helper/wrapper');
 const Logger = require('./helper/logger');
 const Error = require('./error');
-const HtppStatus = require('./http-status/status_code')
+const HtppStatus = require('./http-status/status_code');
+const Common = require('./helper/common');
+const Validation = require('./helper/validator');
 module.exports = {
   Wrapper,
   Error,
   ...HtppStatus,
   ...Logger,
+  ...Common,
+  ...Validation
 };
