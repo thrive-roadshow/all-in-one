cc8c971f16d0f9284fd700fff27bcff705a75083
Farid Tri Wicaksono <farid.wicak@gmail.com>
2024-04-23 07:28:35 +0700

    fix: adjustment logger

--- helper/logger.js
+++ helper/logger.js
@@ -1,5 +1,5 @@
-;
 const apm = require('elastic-apm-node');
+const morgan = require('morgan'); 
 const Pino = require('pino')({
   level: process.env.LOG_LEVEL || 'info',
   prettyPrint: process.env.LOG_PRETTY_PRINT === 'true' || false,
@@ -10,45 +10,64 @@ const Pino = require('pino')({
   enabled: process.env.NODE_ENV !== 'test',
   redact: process.env.LOG_REDUCTION_KEYS && JSON.parse(process.env.LOG_REDUCTION_KEYS) // format ['data.*.key', 'path.to.key', 'arrays[*].key']
 });
-const HeaderHelper = require('./header');
 
 const log = (tags, data) => {
   const logs = { tags };
   if (data) {
-    Object.assign(logs, { data },...apm.currentTraceIds);
+    Object.assign(logs, { data ,...apm.currentTraceIds, reqId: req.correlationId()});
   }
 
   Pino.info(logs);
 };
 
-const logRequest = (req, res) => {
-  const timeDiff = process.hrtime(req.startTime);
-  const timeTaken = Math.round((timeDiff[0] * 1e9 + timeDiff[1]) / 1e6);
-  const version = HeaderHelper.getVersion(req);
-  const { channelid, journeyid } = req.headers;
-  const msisdnBeneficiary = req.body?.msisdnBeneficiary?.msisdn;
+const getRealIp = (req) => {
+  if (typeof req.headers['x-original-forwarded-for'] === 'string') {
+    return req.headers['x-original-forwarded-for'].split(',')[0];
+  } else if (typeof req.headers['x-forwarded-for'] === 'string') {
+    return req.headers['x-forwarded-for'].split(',')[0];
+  }
+  return req.socket.remoteAddress;
+};
 
-  const logData = {
-    _id: req.headers.transactionid,
-    msisdn: req.headers['x-msisdn'],
-    msisdnBeneficiary,
-    method: req.method,
-    url: req.originalUrl || req.url,
-    status: res.statusCode,
-    timeTaken,
-    channelid,
-    version,
-    transactionid: req.headers && req.headers.oldtransactionid,
-    journeyId: journeyid,
-    ip: req.headers && req.headers['x-forwarded-for'],
-    endpoint: req.headers && req.headers.endpoint,
-    ...apm.currentTraceIds
-  };
-  
-  return logData;
+const initLogger = () => {
+  return morgan((tokens, req, res) => {
+    const urlOriginal = `${tokens.url(req, res)}`;
+    const responseStatus = tokens.status(req, res);
+    const message = `${tokens.method(req, res)} ${urlOriginal} - ${responseStatus}`;
+    const clientIp = getRealIp(req);
+    const timeDiff = process.hrtime(req.startTime);
+    const timeTaken = Math.round((timeDiff[0] * 1e9 + timeDiff[1]) / 1e6);
+    const meta = {
+      'service.name': serviceName.service,
+      'service.version': serviceName.version,
+      'log.logger': 'restify',
+      tags: ['audit-log'],
+      'url.original': urlOriginal,
+      'http.request.method': tokens.method(req, res),
+      'user_agent.original': tokens['user-agent'](req, res),
+      'http.response.status_code': responseStatus,
+      'http.response.body.bytes': tokens.res(req, res, 'content-length'),
+      'event.duration': parseInt(tokens['response-time'](req, res, '0')) * 1000000, // in milisecond (ms) so need to convert to ns
+      'http.response.date': tokens.date(req, res, 'iso'),
+      'client.address': req.socket.remoteAddress,
+      'client.ip': clientIp,
+      timeTaken,
+      'user.id': req.userId || '',
+      'user.roles': req.role ? [req.role] : undefined,
+    };
+    const obj = {
+      context: 'service-info',
+      scope: 'audit-log',
+      message: message,
+      meta: meta,
+      ...apm.currentTraceIds
+    };
+    Pino.info(obj);
+    return;
+  });
 };
 
 module.exports = {
   log,
-  logRequest
+  initLogger
 };
